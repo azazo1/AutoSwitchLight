b7e004bc974324b27383276a10e859661def834c
azazo1 <azazo1@qq.com>
2024-02-05 01:02:45 +0800

    测试中，将28byj-48替换为sg90舵机，操作灵活迅速

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,22 @@
 //
 #include <Arduino.h>
 #include <BLEDevice.h>
-
-#define SERVICE_UUID "0000000000084180"
-#define CHAR_UUID "0000000000084181"
-#define DESCRIPTOR_UUID "0000000000084182"
-#define MAX_UNAVAILABLE_CNT 10 // 关灯前扫描无果忍耐次数
+// 我的单片机蓝牙地址为 A0:A3:B3:29:D4:92
+#define SERVICE_UUID "9f867bb8-d8a6-43b2-b129-fefc96fcf689"
+#define CHAR_UUID "7c6473b1-0164-4842-be7f-d1cc945eaae6"
+#define DESCRIPTOR_UUID "08fc14c5-2002-4a9d-8672-15517c70697a"
+#define MAX_UNAVAILABLE_CNT 1 // 关灯前扫描无果忍耐次数
 #define OUTPUT_LED_PIN 23 // 记得LED要串联一个电阻，不然烧坏
 #define MOTOR_PIN1 32
 #define MOTOR_PIN2 33
 #define MOTOR_PIN3 25
 #define MOTOR_PIN4 26
 #define MOTOR_ROTATE 1000 // 马达旋转系数，越大马达一次转动的角度越大
+// sg90 控制舵机角度的值（占PWM周期的40份之n）https://blog.csdn.net/qq_41873236/article/details/116353829
+#define SERVO_STATE_C 2 // 关灯状态
+#define SERVO_STATE_O 4 // 开灯状态
+#define SERVO_STATE_N 3 // 平常状态
+#define SERVO_RESET_TIME 700 // 舵机进入开关灯状态的持续时间（毫秒）
 
 const std::vector<std::string> expected = {
         "iphone 4s",
@@ -32,6 +37,8 @@ unsigned int unavailableCnt = 0;
 // 最终的开关灯决断
 bool output = false;
 unsigned int motorRotationCnt = 0;
+unsigned long servoEnableTime = 0; // 舵机进入开关灯状态的初始时间
+
 
 class MyConnectingCallBack : public BLEServerCallbacks {
 public:
@@ -70,9 +77,7 @@ void IRAM_ATTR motorRoutine() {
         return;
     }
     index++;
-    if (index < 0) {
-        index += 8;
-    } else if (index >= 8) {
+    if (index >= 8) {
         index -= 8;
     }
     unsigned char trueIndex = output ? index : 7 - index; // 正向和反向转动
@@ -80,6 +85,57 @@ void IRAM_ATTR motorRoutine() {
     digitalWrite(MOTOR_PIN2, steps[trueIndex][1]);
     digitalWrite(MOTOR_PIN3, steps[trueIndex][2]);
     digitalWrite(MOTOR_PIN4, steps[trueIndex][3]);
+}
+
+void IRAM_ATTR servoRoutine() {
+    static unsigned char index = 0;
+    static const unsigned char maxIndex = 30;
+    index++;
+    if (index >= maxIndex) {
+        index = 0;
+    }
+//    if (millis() - servoEnableTime > SERVO_RESET_TIME) { // 一定时间后恢复平常状态
+//        if (index >= SERVO_STATE_N) {
+//            digitalWrite(MOTOR_PIN1, LOW);
+//        } else {
+//            digitalWrite(MOTOR_PIN1, HIGH);
+//        }
+//    } else if (output) {
+//        if (index >= SERVO_STATE_O) {
+//            digitalWrite(MOTOR_PIN1, LOW);
+//        } else {
+//            digitalWrite(MOTOR_PIN1, HIGH);
+//        }
+//    } else {
+//        if (index >= SERVO_STATE_C) {
+//            digitalWrite(MOTOR_PIN1, LOW);
+//        } else {
+//            digitalWrite(MOTOR_PIN1, HIGH);
+//        }
+//    }
+    // for test:
+    if (index >= remoteSwitch) {
+        digitalWrite(MOTOR_PIN1, HIGH);
+    } else {
+        digitalWrite(MOTOR_PIN1, LOW);
+    }
+}
+
+void enableServo() {
+    servoEnableTime = millis();
+}
+
+/**
+ * 初始化舵机
+ * 注意不能和initMotor同时使用
+ * */
+void initServo() {
+    pinMode(MOTOR_PIN1, OUTPUT);
+
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, servoRoutine, true);
+    timerAlarmWrite(timer, 100, true); // 0.1ms一格，3ms一个周期
+    timerAlarmEnable(timer);
 }
 
 void initMotor() {
@@ -162,8 +218,10 @@ void setup() {
     Serial.println("Init scan completed");
     initBLEServer();
     Serial.println("Init BLEServer completed");
-    initMotor();
-    Serial.println("Init motor completed");
+//    initMotor();
+//    Serial.println("Init motor completed");
+    initServo();
+    Serial.println("Init servo completed");
     pinMode(OUTPUT_LED_PIN, OUTPUT);
     digitalWrite(OUTPUT_LED_PIN, LOW);
     Serial.println("Init completed");
@@ -180,7 +238,7 @@ void loop() {
         if (unavailableCnt >= MAX_UNAVAILABLE_CNT) {
             output = false;
             digitalWrite(OUTPUT_LED_PIN, LOW);
-            remoteSwitch = 1; // 重置远程开关
+            remoteSwitch = remoteSwitch ? remoteSwitch : 1; // 重置远程开关为true
         }
     } else if (remoteSwitch) {
         unavailableCnt = 0;
@@ -192,7 +250,8 @@ void loop() {
         digitalWrite(OUTPUT_LED_PIN, LOW);
     }
     if (output != rawOutput) {
-        startMotor();
+//        startMotor();
+        enableServo();
     }
     Serial.print("Output: ");
     Serial.println(output);
